ed786c079f05987c1202fcb9c3e83bf9f2b47375
GTK-kala <gtkkala1110@gmail.com>
2025-09-01 23:22:16 +0300

    Stayling navbar

--- Frontend-App/src/components/Navbar/NavBar.jsx
+++ Frontend-App/src/components/Navbar/NavBar.jsx
@@ -1,19 +1,21 @@
 import {Images} from '../../assets/data'
-
+import './NavBar.css'
 const NavBar = () => {
   return (
     <div className="container">
         <div className="left-container">
             <img src={Images.option_icon} alt="" />
-            <p>ProTasks</p>
+            <h2>ProTasks</h2>
         </div>
         <div className="center-container">
            <h1>Tasks</h1>
         </div>
         <div className="right-container">
            <div className='inner-container'>
-             <img src={Images.add_icon} alt="" />
-             <button>Create Tasks</button>
+             <button> 
+                <img src={Images.add_icon} alt="" />
+                <p>Create Tasks</p>
+             </button>
            </div>
         </div>
     </div>
