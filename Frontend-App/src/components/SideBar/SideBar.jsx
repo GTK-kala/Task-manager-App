d955f6fc1febfd8b8807544644c66fad0b935d7c
GTK-kala <gtkkala1110@gmail.com>
2025-09-02 21:37:09 +0300

    SideBar continued

--- Frontend-App/src/components/SideBar/SideBar.jsx
+++ Frontend-App/src/components/SideBar/SideBar.jsx
@@ -1,13 +1,14 @@
-import { FaCheckSquare, FaRegSquare, FaTh } from "react-icons/fa";
-import { FaToggleOn, FaToggleOff, FaCog } from "react-icons/fa";
-import { Images } from "../../assets/data"
+import { FaCheckSquare , FaTh , FaFacebookMessenger , FaDiscord , FaSlack} from "react-icons/fa";
+import { FaGithub, FaToggleOn , FaToggleOff , FaCog , FaMoon} from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
 import './SideBar.css'
 
 const SideBar = () => {
   return (
+/*------------------------Main-container ------------------------ */
     <div className='side-container'>
-        {/* <h2>Task Manager</h2> */}
         <div className="sub-container">
+{/*-----------------------Menu-container-------------------------*/}
             <div className="menu-container">
                <p className="menu-title">MENU</p>
                <ul>
@@ -21,22 +22,42 @@ const SideBar = () => {
                    </li>
                </ul>
             </div>
+{/*-----------------------Message-container-------------------------*/}
             <div className="message-container">
                <p>MESSAGE</p>
                <ul>
-                   <li>Message 1</li>
-                   <li>Message 2</li>
-                   <li>Message 3</li>
+                   <li>
+                    <FaGithub />
+                    <span> GitHub</span>
+                   </li>
+                   <li>
+                    <FaFacebookMessenger />
+                    <span>Messenger</span>
+                   </li>
+                   <li>
+                    <SiGmail />
+                    <span>Gmail</span>
+                   </li>
+                   <li>
+                    <FaSlack />
+                    <span>Slack</span>
+                   </li>
+                   <li>
+                    <FaDiscord />
+                    <span>Discord</span>    
+                   </li>
                </ul>
             </div>
+{/*-----------------------Settings-container-------------------------*/}
             <div className="settings-container">
                 <p>
                     <FaCog className="settings-icon"/>
                     <span>Settings</span>
                 </p>
-                <p>
-                    <FaToggleOn className="toggle-icon"/>
-                    <span>Dark Mode</span>
+                <p className="dark-mode-container">
+                    <FaMoon />
+                     <span className="dark-mode-icon">Dark Mode</span>
+                    <FaToggleOn className="toggle-icon"/>  
                 </p>
             </div>
         </div>
