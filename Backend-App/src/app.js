8eb95cc82124d7b0bea3fbad60c9dae6c7f68de8
GTK-kala <gtkkala1110@gmail.com>
2025-09-05 21:11:38 +0300

    Creating Tables

--- Backend-App/src/app.js
+++ Backend-App/src/app.js
@@ -4,12 +4,8 @@ import multer from 'multer'
 
 
 const port = 3001;
-const app = express();
+export const app = express();
 
 app.listen(port, ()=>{
     console.log(`app is listening on port ${port}`)
-});
-
-app.get('/',(req,res) =>{
-  res.send('hello this is express server');
 });
--- Backend-App/src/config/db.js
+++ Backend-App/src/config/db.js
@@ -1,3 +1,4 @@
+import { app } from '../app.js';
 import mysql2 from 'mysql2'
 
 const connection = mysql2.createConnection({
@@ -15,4 +16,46 @@ connection.connect((err) =>{
     }
 });
 
+const createTableUser =`CREATE TABLE USERS(
+                  user_id INT AUTO_INCREMENT PRIMARY KEY,
+                  username VARCHAR(100) not null,
+                  email VARCHAR(100) not null,
+                  password VARCHAR(255) not null
+                )`;
+
+                connection.query(createTableUser, (err, result) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log("Table created");
+                    }
+                });
+    const createTableTask =`CREATE TABLE TASKS(
+                  task_id INT AUTO_INCREMENT PRIMARY KEY,
+                  user_id INT,
+                  title VARCHAR(100) not null,
+                  description TEXT not null,
+                  status ENUM('pending', 'in_progress', 'completed') not null,
+                  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                  FOREIGN KEY (user_id) REFERENCES USERS(user_id)
+                )`;
+
+                connection.query(createTableTask, (err, result) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log("Table created");
+                    }
+                });
+                connection.query(createTableTask, (err, result) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log("Table created");
+                    }
+                });
+app.get('/',(req,res) =>{
+    res.send('Table is created');
+})
 export default connection;
