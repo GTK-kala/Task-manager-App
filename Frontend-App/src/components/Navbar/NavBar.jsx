3b2020e6f587fb518927a610e092bfaf8881abbf
GTK-kala <gtkkala1110@gmail.com>
2025-09-08 14:16:58 +0300

    Making NavBar Responsive

--- Frontend-App/src/components/Navbar/NavBar.jsx
+++ Frontend-App/src/components/Navbar/NavBar.jsx
@@ -1,72 +1,63 @@
+import { useState } from "react";
+import { FaBars, FaPlus, FaUserCircle } from "react-icons/fa";
 import { MdNotifications, MdMessage } from "react-icons/md";
-import { FaPlus, FaBars, FaUserCircle  } from "react-icons/fa";
-import { Images } from "../../assets/data";
-import { useContext, useEffect, useState } from "react";
-import { contextApi } from "../../context/Contextprovider";
-import { useNavigate } from "react-router-dom"
-import "./NavBar.css";
-const NavBar = () => {
-  const navigate = useNavigate();
-  const { ToggleHandel, toggle, isOpen  , ToggleDisplay} = useContext(contextApi);
-  const [menu, setMenu] = useState(false);
+import { useNavigate } from "react-router-dom";
+import { Images} from "../../assets/data" 
+import './NavBar.css'
 
-  useEffect(() => {
-      if (window.innerWidth > 768) {
-          setMenu(true);
-      }else{
-        setMenu(false);
-      }
-  }, []);
+const  Navbar = () => {
+  const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
 
   return (
-/*---------------------------------- container -----------------------------*/
-    <div className="container">
-{/* ---------------------------- left-container ----------------------------*/}
+    <div className="navbar-container">
+      {/* ---------------- Left ---------------- */}
       <div className="left-container" onClick={() => navigate("/")}>
         <img src={Images.option_icon} alt="" />
         <h2>ProTasks</h2>
       </div>
-{/* --------------------------- center-container ---------------------------*/}
+
+      {/* ---------------- Center ---------------- */}
       <div className="center-container">
         <h1>Tasks</h1>
       </div>
-{/* --------------------------- right-container ----------------------------*/}
-      {menu ? (
-        <div className={`right-container ${toggle}`}>
-          <div className={`inner-container ${isOpen}`}>
-            <button>
-              <FaPlus className="icon" />
-              <p>Create Tasks</p>
-            </button>
-            <div className="icon-container">
-              <div className="icon-wrapper">
-                <MdNotifications className="icon bell" />
-                <span className="badge">3</span>
-              </div>
-              <div className="icon-wrapper">
-                <MdMessage className="icon message" />
-                <span className="badge">5</span>
-              </div>
-              <div className="icon-wrapper" onClick={() => {navigate("/account"), ToggleDisplay()}}>
-                <FaUserCircle className="icon message" />
-              </div>
+
+      {/* ---------------- Right (menu) ---------------- */}
+      <div className={`right-container ${isOpen ? "open" : ""}`}>
+        <div className="inner-container">
+          <button>
+            <FaPlus className="icon" />
+            <p>Create Tasks</p>
+          </button>
+          <div className="icon-container">
+            <div className="icon-wrapper">
+              <MdNotifications className="icon bell" />
+              <span className="badge">3</span>
+            </div>
+            <div className="icon-wrapper">
+              <MdMessage className="icon message" />
+              <span className="badge">5</span>
+            </div>
+            <div
+              className="icon-wrapper"
+              onClick={() => {
+                navigate("/account");
+              }}
+            >
+              <FaUserCircle className="icon user" />
             </div>
           </div>
         </div>
-      ) : (
-        <> </>
-      )}
-{/* ---------------------------- menu-icon -----------------------------*/}
-      <div className="menuBar-icon">
-        <FaBars
-          className="icon"
-          onClick={() => {
-            ToggleHandel(), setMenu(!menu);
-          }}
-        />
+      </div>
+
+      {/* ---------------- Hamburger ---------------- */}
+      <div className="menuBar-icon" onClick={toggleMenu}>
+        <FaBars className="icon" />
       </div>
     </div>
   );
-};
+}
 
-export default NavBar;
+export default Navbar
