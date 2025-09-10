575f0b05460044aad9d35570368a719d684a2552
GTK-kala <gtkkala1110@gmail.com>
2025-09-11 00:18:55 +0300

    setup form for Tasks

--- Frontend-App/src/components/Tasks/Task.jsx
+++ Frontend-App/src/components/Tasks/Task.jsx
@@ -3,10 +3,24 @@ import "./Task.css";
 const Task = () => {
   return (
     <>
-      <div>
-        <h2>Task Title</h2>
-        <p>Task Description</p>
-      </div>
+      <form className="task-form">
+{/* Task Title */}
+        <input type="text" placeholder="Task title" required />
+
+{/* Description */}
+        <textarea placeholder="Task description" />
+
+{/* Status */}
+        <select value={''}>
+          <option value="pending">Pending</option>
+          <option value="in-progress">In Progress</option>
+          <option value="completed">Completed</option>
+        </select>
+{/* User ID (hidden for single-user app, select for multi-user app) */}
+        <input type="hidden" />
+
+        <button type="submit">Create Task</button>
+      </form>
     </>
   );
 };
