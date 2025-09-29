968bd19e7aeabfc6047fe28c887fdf18c98a9424
GTK-kala <gtkkala1110@gmail.com>
2025-09-29 17:42:42 +0300

    Testing the Api of users

--- Backend-App/src/controllers/user.js
+++ Backend-App/src/controllers/user.js
@@ -1,16 +1,23 @@
+const user  = [];
+
 export const AddUsers  = (req , res) =>{
    const {name , email , password} = req.body;
+   let id = user.length;
+   
     if(!req.body){
         res.status(400).json({
             message : 'please full fill the requirements'
         })
     };
+    user.push()
     res.status(200).json({
         name : name,
         email : email,
         password : password
     });
-    console.log('name =' + name);
-    console.log('email =' + email);
-    console.log('password =' + password);
+   
 };
+
+export const upDateUser = (req,res) =>{
+  
+}
