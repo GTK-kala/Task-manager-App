838602a64e5b9fe02124de7404172820636d9856
GTK-kala <gtkkala1110@gmail.com>
2025-10-07 16:13:50 +0300

    Adding Tasks to UI from The Database Using API

--- Frontend-App/src/components/DisplayTasks/Tasks.jsx
+++ Frontend-App/src/components/DisplayTasks/Tasks.jsx
@@ -1,11 +1,36 @@
-import { Tasks as tasks } from "../../assets/data";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { toast } from "react-toastify";
 import "./Tasks.css";
 
 const Tasks = () => {
-  return (
+  const [taskList, setTaskList] = useState([]);
+  const navigate = useNavigate();
+
+  const fetchTasks = async () => {
+    try {
+      const response = await fetch("http://localhost:3001/api/tasks");
+      const data = await response.json();
+      setTaskList(data.results);
+    } catch (error) {
+      console.error("Error fetching tasks:", error);
+    }
+  };
+
+  const FilterTasks = (id) => {
+    setTaskList(prevTasks => prevTasks.filter(task => task.id !== id));
+    toast.success("Task deleted successfully");
+    console.log(id);
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  return taskList.length > 0 ? (
     <div className="tasks-container">
-      {tasks.map((task) => (
-        <div className={`task-card ${task.status}`} key={task.task_id}>
+      {taskList.map((task, i) => (
+        <div className={`task-card ${task.status}`} key={i}>
           <div className="task-header">
             <h3>{task.title}</h3>
             <span className="status">{task.status}</span>
@@ -21,13 +46,19 @@ const Tasks = () => {
                 : "No deadline"}
             </small>
             <div className="task-actions">
-              <button className="edit">✏️</button>
-              <button className="delete">🗑️</button>
+              <button className="edit" onClick={() => navigate("/TaskPage")}>
+                ✏️
+              </button>
+              <button className="delete" onClick={() => FilterTasks(task.task_id)}>
+                🗑️
+              </button>
             </div>
           </div>
         </div>
       ))}
     </div>
+  ) : (
+    <p>Page not found</p>
   );
 };
 
--- Frontend-App/src/pages/DisplayTask.jsx
+++ Frontend-App/src/pages/DisplayTask.jsx
@@ -1,8 +1,11 @@
 import Tasks from '../components/DisplayTasks/Tasks'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 const DisplayTask = () => {
   return (
     <>
       <Tasks />
+      <ToastContainer  position='top-right' autoClose={1000}/>
     </>
   )
 }
